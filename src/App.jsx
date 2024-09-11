2bab3222a64b0dbcf944a83351f800079fc4e64a
Bilal <mohammadbilal91112@gmail.com>
2024-09-12 04:16:36 +0500

    working

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+
+
+
+function App() {
+    return (
+      <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4">
+          <h1 className="text-2xl font-bold text-blue-600">Welcome to Tailwind CSS!</h1>
+          <p className="text-gray-700">
+            This is a simple React component styled with Tailwind CSS. You can use utility classes to style elements directly in your JSX.
+          </p>
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+            Click Me
+          </button>
+        </div>
+      </div>
+    );
+  }
+  
+  export default App;
+  
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,47 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Service from './pages/Service.jsx'
+import Singlepage from './pages/Singlepage.jsx'
+
+const router = createBrowserRouter([
+  {
+    pat : '/',
+    element : <Layout />,
+    children : [
+      {
+        path : '',
+        element : <Home />
+      },
+      {
+        path : 'About',
+        element : <About />
+      },
+      {
+        path : 'Contact',
+        element : <Contact /> 
+      },
+      {
+        path : 'Service',
+        element : <Service/>
+      },
+      {
+        path : 'Singlepage/:id',
+        element : <Singlepage />
+      },
+      {
+        path : '*',
+        element : <h1>404 Not Found</h1>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-    <App />
+  <RouterProvider router={router}>
+  </RouterProvider>
 )
