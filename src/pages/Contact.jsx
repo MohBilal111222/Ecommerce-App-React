e1766b1f9a4cb721caf0b5dcb3081866d3515da4
Bilal <mohammadbilal91112@gmail.com>
2024-09-14 04:50:27 +0500

    its change

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ function Contact() {
       <>
       <h1 className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500">Contact Us !</h1>
       
-      
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolore architecto minima reprehenderit aliquid adipisci! Maiores in sapiente, facere tempore at tenetur repudiandae quaerat quasi, voluptates error quidem commodi obcaecati.</p>
       </>
     )
   }
--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -4,6 +4,12 @@ function Service() {
     return (
       <>
       <h1 className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500">Service</h1>
+
+
+
+
+
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non dignissimos numquam, deleniti nostrum fugit error ipsa rerum eum sint ipsam!</p>
       </>
     )
   }
